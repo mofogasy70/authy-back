41bc8a307ad4f3cef7ef43b031f69ac24028efba
mofogasy70 <99830988+mofogasy70@users.noreply.github.com>
2024-05-14 17:26:47 +0300

    soket

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-// index.ts
 import express from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
 import connectDB from './config/db';
 import apiRouter from './routes/API.routes';
 import cors from 'cors';
@@ -10,16 +11,37 @@ import apiExterneRouter from './routes/API-Externe.routes';
 import UserController from './module/Account/User/User.controller';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
+
+// Connexion à la base de données et démarrage du serveur Express
 connectDB().then(() => {
   Main.main();
 });
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
+
+// Routes
 app.use('/API', authenticateToken, apiRouter);
-app.use('/', apiExterneRouter)
-app.use('/home',UserController.test)
-app.listen(APP_PORT, () => {
-  console.log(`->Serveur en cours d'exécution sur le port ${APP_PORT}`);
+app.use('/', apiExterneRouter);
+app.use('/home', UserController.test);
+
+// Gestion des connexions Socket.io
+io.on('connection', (socket: Socket) => {
+  socket.on('notification', (data) => {
+    try {
+      console.log(data);
+      socket.emit("test", { type: data.type, message: data.message });
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
 
+// Démarrage du serveur
+server.listen(APP_PORT, () => {
+  console.log(`->Serveur en cours d'exécution sur le port ${APP_PORT}`);
+});
--- src/module/Application/application/Application.service.ts
+++ src/module/Application/application/Application.service.ts
@@ -6,9 +6,9 @@ import User from "../../Account/User/User.model";
 import Role from "../../Account/Role/Role.model";
 
 class ApplicationService {
-    async getApplication(UserId:string) {
+    async getApplication(UserId: string) {
         try {
-            const valiny =await Application.find({ AppId:{ $ne: "d092530a-1386-4b90-9769-fcb4a38c477c"},authors:new mongoose.Types.ObjectId(UserId) }).populate("Categorie").populate("Platform");
+            const valiny = await Application.find({ AppId: { $ne: "d092530a-1386-4b90-9769-fcb4a38c477c" }, authors: new mongoose.Types.ObjectId(UserId) }).populate("Categorie").populate("Platform");
             console.log(valiny);
             return valiny;
         } catch (error) {
@@ -23,22 +23,23 @@ class ApplicationService {
             throw error;
         }
     }
-    async createApplication(DomainName: string, User: string, Uri: string, Logo: string,Categorie:mongoose.Types.ObjectId, Platform:mongoose.Types.ObjectId, UriRedirection:string) {
+    async createApplication(DomainName: string, User: string, Uri: string, Logo: string, Categorie: mongoose.Types.ObjectId, Platform: mongoose.Types.ObjectId, UriRedirection: string) {
         try {
             const AppId = await uuidv4();
-            const AppKey =await uuidv4();
+            const AppKey = await uuidv4();
             const ApplicationTemp = new Application({
                 DomainName,
-                AppId ,
+                AppId,
                 Uri,
                 Logo,
-                createdAt:new Date(),
-                status:1,
-                UriRedirection:UriRedirection,
-                Platform:Platform,
-                Categorie:Categorie,
-                AppKey:AppKey,
-                authors:User
+                createdAt: new Date(),
+                status: 1,
+                UriRedirection: UriRedirection,
+                Platform: Platform,
+                Categorie: Categorie,
+                AppKey: AppKey,
+                authors: User,
+                securityStatus: false
             });
             return await ApplicationTemp.save();
         } catch (error) {
@@ -113,21 +114,21 @@ class ApplicationService {
             throw error;
         }
     }
-    async search(status: number, start: Date, end: Date, domainName: string,UserId:string,NameId:string,sort:string) {
+    async search(status: number, start: Date, end: Date, domainName: string, UserId: string, NameId: string, sort: string) {
         try {
-            const user=await User.findById(UserId);
-            const role=await Role.findOne({Name:"Admin"});
+            const user = await User.findById(UserId);
+            const role = await Role.findOne({ Name: "Admin" });
             let monObjet: { [key: string]: any } = {};
-            let sortobj:any;
-            if (status) {monObjet.status= {status:status}}
+            let sortobj: any;
+            if (status) { monObjet.status = { status: status } }
             monObjet.AppId = { $ne: "d092530a-1386-4b90-9769-fcb4a38c477c" };
             if (NameId) {
-                monObjet.$or=[
-                    { DomainName: { $regex: new RegExp(NameId, 'i') } }, 
+                monObjet.$or = [
+                    { DomainName: { $regex: new RegExp(NameId, 'i') } },
                     { AppId: { $regex: new RegExp(NameId, 'i') } },
-                  ]
+                ]
             }
-            if (user.Role!==role._id) {
+            if (user.Role !== role._id) {
                 monObjet.authors = new mongoose.Types.ObjectId(UserId);
             }
             if (start && end) {
@@ -137,14 +138,14 @@ class ApplicationService {
                 end && (monObjet.createdAt = { $lt: end });
             }
             domainName && (monObjet.DomainName = { $regex: new RegExp(domainName, "i") })
-            let valiny =await Application.find(monObjet).sort({"DomainName":1}).populate("Categorie").populate("Platform");
+            let valiny = await Application.find(monObjet).sort({ "DomainName": 1 }).populate("Categorie").populate("Platform");
             return valiny;
         } catch (error) {
             throw error;
         }
     }
     async getLogo(Name: string): Promise<string> {
-        const imagePath = path.join('src/module/Logo/'+Name);
+        const imagePath = path.join('src/module/Logo/' + Name);
         return imagePath;
     }
 }
